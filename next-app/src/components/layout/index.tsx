98ac78d738a047fdcdf9640c8c155043a557fb1b
Natapon Pantuwong <pantuwongn@gmail.com>
2023-01-12 22:54:52 +0700

    fix spinner css

--- next-app/src/components/layout/index.tsx
+++ next-app/src/components/layout/index.tsx
@@ -14,7 +14,7 @@ const Container: FC<IProps> = ({ title, children }: IProps) => {
     <Spin spinning={isLoading} size="large" style={{ top: '50%', transform: 'translateY(-50%)' }}>
       <Header title={title}/>
       <div className="w-full overflow-y-auto overflow-x-hidden" style={{ height: "calc(100vh - 64px)" }}>
-        <div className="w-full h-full p-4 m-auto">
+        <div className="w-full h-screen p-4 m-auto">
           {children}
         </div>
       </div>
