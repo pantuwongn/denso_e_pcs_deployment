d71d1cf3d21094d1c5c7e1980814796cbe8e5806
Sahapat <sahapat.starcaft@gmail.com>
2023-01-12 23:25:29 +0700

    Revert: api

--- next-app/src/actions/download.action.ts
+++ next-app/src/actions/download.action.ts
@@ -2,12 +2,12 @@ import axiosInstance from "@/lib/axios";
 import { FormJsonResponse, PCSUploadForm } from "@/types/upload.type";
 
 export async function getFormJson(): Promise<FormJsonResponse> {
-  const { data } = await axiosInstance.get<FormJsonResponse>('/get_mock_data')
+  const { data } = await axiosInstance.get<FormJsonResponse>('/api/get_mock_data')
   return data
 }
 
 export async function convertJsonToXlsx(uploadForm: PCSUploadForm): Promise<Blob> {
-  const { data } = await axiosInstance.post<Blob>('/convert_json_to_xlsx', uploadForm, {
+  const { data } = await axiosInstance.post<Blob>('/api/convert_json_to_xlsx', uploadForm, {
     responseType: 'blob'
   })
   return data
